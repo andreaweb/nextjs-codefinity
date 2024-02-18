f7e2abec63b197cd340390e8f0e54d83a92b6c56
Andrea <return.santana@gmail.com>
2024-02-18 20:12:00 +0100

    showing some basic info on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,26 @@
-export default function Page() {
-    return <p className="text-white">Dashboard Page</p>;
-  }
-    
+// ...
+import { poppins } from '@/app/ui/fonts';
+import { fetchIncome } from '@/app/lib/data';
+import IncomeChart  from '@/app/ui/dashboard/income-chart'
+
+export default async function Page() {
+  const income = await fetchIncome();
+
+  return (
+    <main className="rounded-xl bg-neutral-900 p-6">
+      <h1
+        className={`${poppins.className} mb-4 text-center text-xl text-white md:text-3xl`}
+      >
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* ... */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <IncomeChart income={income} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
+}
+  
